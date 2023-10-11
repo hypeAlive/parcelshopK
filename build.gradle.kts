2c0cf67d85d0cda8bee073351cee3f81dae93682
Jean-Paul Edoh <jean-paul.edoh@gls-ecl>
2023-10-11 09:03:09 +0200

    remove dto and service stub & adjust build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.8"
     id("io.spring.dependency-management") version "1.0.14.RELEASE"
-    java
     kotlin("jvm") version "1.9.10"
 }
 
--- src/main/kotlin/com/gls/parcelshop/service/ParcelService.kt
+++
@@ -1,7 +0,0 @@
-package com.gls.parcelshop.service
-
-import org.springframework.stereotype.Service
-
-@Service
-class ParcelService {
-}
