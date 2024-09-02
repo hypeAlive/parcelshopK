94debc9c758cfac572afe354abc45aab147d1560
nicolas <nicolasfr.privat@gmail.com>
2024-09-02 12:05:23 +0200

    adds functions to get Parcels by DeliveryState

--- src/main/kotlin/com/gls/parcelshop/repository/ParcelRepository.kt
+++ src/main/kotlin/com/gls/parcelshop/repository/ParcelRepository.kt
@@ -1,5 +1,6 @@
 package com.gls.parcelshop.repository
 
+import com.gls.parcelshop.model.DeliveryState
 import com.gls.parcelshop.model.Parcel
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
@@ -8,4 +9,6 @@ import org.springframework.stereotype.Repository
 interface ParcelRepository : CrudRepository<Parcel, Long> {
     override fun findAll(): List<Parcel>
     fun findAllByParcelNumber(parcelNumber: String): List<Parcel>
+    fun findAllByDeliveryDateAndDeliveryState(deliveryDate: String, deliveryState: DeliveryState): List<Parcel>
+    fun findAllByDeliveryState(deliveryState: DeliveryState): List<Parcel>
 }
